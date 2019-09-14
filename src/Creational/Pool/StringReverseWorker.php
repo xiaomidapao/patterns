f9083bdb073193c46c68dea314168e1bbf0d12ab
guangyuan <834421992@qq.com>
2019-09-14 17:29:18 +0800

    对象池模式

---
+++ src/Creational/Pool/StringReverseWorker.php
@@ -0,0 +1,16 @@
+<?php
+namespace DesignPatterns\Creational\Pool;
+
+class StringReverseWorker
+{
+	private $createdAt;
+	public function __construct()
+	{
+		$this->createdAt = new \DateTime();
+	}
+
+	public function run(string $text)
+	{
+		return strrev($text);
+	}
+}
